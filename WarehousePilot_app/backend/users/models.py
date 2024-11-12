3f81a860947c57fab4f37c0e2724d58a74ce557c
Megan Coscia <meg.coscia@gmail.com>
2024-11-11 23:25:45 -0500

    setup database model for user information to be displayed #41

--- WarehousePilot_app/backend/users/models.py
+++ WarehousePilot_app/backend/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.pardir, 'auth_app')))
+from auth_app.models import users as users_table
+
+class staff(models.Model):
+    user_id = models.AutoField(primary_key=True)               
+    first_name = models.CharField(max_length=100)
+    last_name= models.CharField(max_length=100)
+    staff_id = users_table.user_id
+    user_id = users_table.user_id
+    email = users_table.email
+    role = users_table.role
+    designation = models.CharField(max_length=100)
