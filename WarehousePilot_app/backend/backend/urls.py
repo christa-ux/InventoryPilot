4f26c24c12d1df5ce7aeaef1dc8931e4a8b218ec
Irisvella <122749049+Irisvella@users.noreply.github.com>
2024-12-14 20:37:03 -0500

    Added a missing comma

--- WarehousePilot_app/backend/backend/urls.py
+++ WarehousePilot_app/backend/backend/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
 #    path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
     path('admin_dashboard/', include('admin_dashboard.urls')),
-    path('manager_dashboard/', include('manager_dashboard.urls'))
+    path('manager_dashboard/', include('manager_dashboard.urls')),
     path('orders/', include('orders.urls')),
     path('parts/', include('parts.urls')),
     path('manufacturingLists/', include('manufacturingLists.urls')),
