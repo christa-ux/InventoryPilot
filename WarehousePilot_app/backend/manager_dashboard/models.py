f75689bc2da04c5670120ae49920557ae9104ed7
yousefenein <37770446+yousefenein@users.noreply.github.com>
2024-12-29 16:59:30 -0500

    Revert "sku-level-backend-1.0-issue #26"

--- WarehousePilot_app/backend/manager_dashboard/models.py
+++ WarehousePilot_app/backend/manager_dashboard/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from inventory.models import Inventory
 
 # Create your models here.
-class Alert(models.Model):
-    inventory_item = models.ForeignKey(Inventory, on_delete=models.CASCADE)
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Alert for {self.inventory_item.sku_color.sku_color} at {self.inventory_item.location}"
