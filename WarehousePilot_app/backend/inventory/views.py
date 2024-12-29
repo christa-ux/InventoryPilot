95b32a5bdd08fecba85a9d844afc17833b656ef0
yousefenein <yousefenein@outlook.com>
2024-12-28 23:43:19 -0500

    Added Stock level indicator for sample thresholds for now. These are to be adjusted but are as follows :
    0 = out of stock
    < 50 = low stock
    between 50 - 100 = moderate stock
    > 100 is high stock

--- WarehousePilot_app/backend/inventory/views.py
+++ WarehousePilot_app/backend/inventory/views.py
@@ -4,6 +4,17 @@ from .models import Inventory
 def get_inventory(request):
     try:
         inventory_data = Inventory.objects.all().values()
-        return JsonResponse(list(inventory_data), safe=False)
+        inventory_list = list(inventory_data)
+        for item in inventory_list:
+            qty = item['qty']
+            if qty == 0:
+                item['status'] = 'Out of Stock'
+            elif qty < 50:
+                item['status'] = 'Low'
+            elif 50 <= qty <= 100:
+                item['status'] = 'Moderate'
+            else:
+                item['status'] = 'High'
+        return JsonResponse(inventory_list, safe=False)
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
