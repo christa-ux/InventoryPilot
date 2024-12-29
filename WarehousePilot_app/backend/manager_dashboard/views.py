8bf8808e508cf74fa7c1d042d916349a49f8bbad
James Bitharas <84352865+strikeyamato@users.noreply.github.com>
2024-12-29 14:02:24 -0500

    Update views.py
    Removed ProfileView, and Changepassworc Api

--- WarehousePilot_app/backend/manager_dashboard/views.py
+++ WarehousePilot_app/backend/manager_dashboard/views.py
@@ -12,28 +12,6 @@ from django.contrib.auth import update_session_auth_hash
 def home(request):
     return HttpResponse("Hello, World!")
 
-class ProfileView(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        try:
-            user = request.user
-            print(f"Request user: {user}")  # Debugging
-            print(f"User type: {type(user)}")  # Debugging
-
-            user_data = {
-                'username': user.username,
-                'email': user.email,
-                'role': getattr(user, 'role', 'N/A'),  
-                'first_name': getattr(user, 'first_name', 'N/A'),
-                'last_name': getattr(user, 'last_name', 'N/A'),
-                'department': getattr(user, 'department', 'N/A'),
-            }
-            return Response(user_data)
-        except Exception as e:
-            return Response({"error": str(e)}, status=500)
-
 class SKUAlertAPIView(APIView):
     def get(self, request):
         try:
@@ -58,22 +36,3 @@ class SKUAlertAPIView(APIView):
         
         except ValueError as e:  # Handle invalid numeric conversions
             return Response({"error": f"Invalid filter value: {str(e)}"}, status=400)
-
-class ChangePasswordView(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        user = request.user
-        old_password = request.data.get('old_password')
-        new_password = request.data.get('new_password')
-
-        if not user.check_password(old_password):
-            return Response({"detail": "Old password is incorrect"}, status=status.HTTP_400_BAD_REQUEST)
-
-        user.set_password(new_password)
-        user.save()
-        update_session_auth_hash(request, user) 
-
-        return Response({"detail": "Password changed successfully"}, status=status.HTTP_200_OK)
-
