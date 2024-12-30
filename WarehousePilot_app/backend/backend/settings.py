47e1af8fb4be21ac4f87c1069771c57a9bc9a193
yousefenein <yousefenein@outlook.com>
2024-12-30 02:48:19 -0500

    alerts are now triggered whenever there is a low stock item and an email is sent to notify. Might get annoying so will look to add a feature to subscribe/unsubscribe to email notifs and just have it display in front end

--- WarehousePilot_app/backend/backend/settings.py
+++ WarehousePilot_app/backend/backend/settings.py
@@ -125,6 +125,19 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# EMAIL 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'wp.notifs@gmail.com'
+EMAIL_HOST_PASSWORD = 'dyur vqgz snpv jagv'  # App password
+DEFAULT_FROM_EMAIL = 'wp.notifs@gmail.com'
+
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
--- WarehousePilot_app/backend/inventory/views.py
+++ WarehousePilot_app/backend/inventory/views.py
@@ -1,5 +1,26 @@
 from django.http import JsonResponse
 from .models import Inventory
+from django.core.mail import send_mail
+from threading import Thread
+import logging
+
+alerted_items = set()
+
+def send_alert(item):
+    if item["sku_color_id"] not in alerted_items:
+        def send_email():
+            try:
+                send_mail(
+                    'Low Stock Alert',
+                    f'The stock for {item["sku_color_id"]} is low. Current quantity: {item["qty"]}.',
+                    'wp.notifs@gmail.com',
+                    ['warehousepilot2024@gmail.com'],
+                    fail_silently=False,
+                )
+            except Exception as e:
+                logging.error(f"Failed to send email for {item['sku_color_id']}: {e}")
+        Thread(target=send_email).start()
+        alerted_items.add(item["sku_color_id"])
 
 def get_inventory(request):
     try:
@@ -11,6 +32,7 @@ def get_inventory(request):
                 item['status'] = 'Out of Stock'
             elif qty < 50:
                 item['status'] = 'Low'
+                send_alert(item)
             elif 50 <= qty <= 100:
                 item['status'] = 'Moderate'
             else:
