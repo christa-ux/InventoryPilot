50d162cbb8b6ccabbe562bbba480418f8a8feac4
Megan Coscia <meg.coscia@gmail.com>
2024-12-31 14:56:00 -0500

    #116 - settting up backend tests for #41 feature

--- WarehousePilot_app/backend/admin_dashboard/tests.py
+++ WarehousePilot_app/backend/admin_dashboard/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from auth_app.models import users
+from .serializers import StaffSerializer
 
-# Create your tests here.
+User = get_user_model()
+
+class ManageUsersViewTest(TestCase):
+    def setUp(self):
+        self.admin_user = users.objects.create_user(
+            first_name='Test',
+            last_name='Admin',
+            username="admin",
+            password="adminpassword",
+            email="admin@example.com",
+            dob='1990-01-01',
+            department='Testing',
+            role='admin'
+        )
+
+        self.staff_user = users.objects.create_user(
+            first_name='Test',
+            last_name='Employee',
+            username="employee",
+            password="employeepassword",
+            email="employee@example.com",
+            dob='2025-12-29',
+            department='Testing',
+            role='user'
+        )
+
+        # link to manage users feature
+        self.manage_users_url = reverse('manage_users')
+
+        # create API client
+        self.client = APIClient()
+
+        # create token for admin user
+        refresh = RefreshToken.for_user(self.admin_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
