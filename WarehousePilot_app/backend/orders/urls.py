6a3593e7c29c8d6eb6c7a6d88b0c22b319184360
Inas Fawzi <inasfawzi24@gmail.com>
2025-01-01 21:45:42 -0500

    creating backend view to generate manufacturing list and inventory picklist of an order #86

--- WarehousePilot_app/backend/orders/urls.py
+++ WarehousePilot_app/backend/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import GenerateInventoryAndManufacturingListsView
 
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('generateLists/', GenerateInventoryAndManufacturingListsView.as_view(), name="generateLists"),
 ]
--- WarehousePilot_app/backend/orders/views.py
+++ WarehousePilot_app/backend/orders/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
 from django.http import HttpResponse
+from rest_framework.response import Response
+from .models import Orders, OrderPart
+from inventory.models import Inventory
+from parts.models import Part
 
+#generates an inventory picklist and a maufacturing list of an order once the order is "started"
+class GenerateInventoryAndManufacturingListsView(APIView):
+    def post(self, request):
+        orderID = request.data.get("orderID")
+        print("GenerateInventoryAndManufacturingListsView")
+        print(f"Response OrderID: {orderID}")
+        order = Orders.objects.get(order_id=orderID)
+        print(f'retrieving order {order.__dict__}')
 
-def index(request):
-    return HttpResponse("Hello, world. You're at inventory index.")
+        
+        '''
+        for p in Part.objects.all():
+            print(p.__dict__)
+        for i in Inventory.objects.all():
+            print(i.__dict__)
+        for op in OrderPart.objects.all():
+            print(op.__dict__)
+        '''
+        return Response({'testResponse':'success'})
