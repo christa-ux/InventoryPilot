135070fbe393ea9113fc3a51a8902a5ce8a25cfe
Megan Coscia <meg.coscia@gmail.com>
2025-01-02 19:29:04 -0500

    #116 - adjust initial test, added test for non-admin user accessing p

--- WarehousePilot_app/backend/admin_dashboard/tests.py
+++ WarehousePilot_app/backend/admin_dashboard/tests.py
@@ -21,7 +21,8 @@ class ManageUsersViewTest(TestCase):
             email="admin@example.com",
             dob='1990-01-01',
             department='Testing',
-            role='admin'
+            role='admin',
+            is_staff=False
         )
 
         self.staff_user = users.objects.create_user(
@@ -32,25 +33,36 @@ class ManageUsersViewTest(TestCase):
             email="employee@example.com",
             dob='2025-12-29',
             department='Testing',
-            role='user'
+            role='user',
+            is_staff=True
         )
 
         self.manage_users_url = reverse('manage_users')
 
         self.client = APIClient()
 
-        refresh = RefreshToken.for_user(self.admin_user)
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
-
-    # test_user_authorization_success(): Check if an admin 
-    def test_user_authorization_success(self):
+    # test_manage_users_authenticated_request(): Expected behaviour when an admin accesses the page
+    def test_manage_users_authenticated_request(self):
         # Arrange
         staff_data = users.objects.all()
         serializer = StaffSerializer(staff_data, many=True)
+        refresh = RefreshToken.for_user(self.admin_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        # Act
+        response = self.client.get(self.manage_users_url)
+        # Assert
+        self.assertEqual(response.status_code, status.HTTP_200_OK) # check if authorized to be on this page
+        self.assertEqual(response.data, serializer.data) # check if page retrieves all entries from the database
+    
+    # test_manage_users_non_admin_user_request(): Handling of a non-authorized role (not an admin)
+    def test_manage_users_non_admin_user_request(self):
+        # Arrange
+        refresh = RefreshToken.for_user(self.staff_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
 
         # Act
         response = self.client.get(self.manage_users_url)
 
         # Assert
-        self.assertEqual(response.status_code, status.HTTP_200_OK) # check if authorized to be on this page
-        self.assertEqual(response.data, serializer.data) # check if page retrieves all entries from the database
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data, {'detail': 'You do not have permission to perform this action.'})
