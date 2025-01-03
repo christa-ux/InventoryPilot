1655af2f353d926e12727c76637e1f7470342c84
yousefenein <yousefenein@outlook.com>
2025-01-02 23:52:02 -0500

    #122 -cUpdated tests to check if it matches with whats in db.

--- WarehousePilot_app/backend/inventory/tests.py
+++ WarehousePilot_app/backend/inventory/tests.py
@@ -24,6 +24,12 @@ class InventoryTests(TestCase):
         response = self.client.get(reverse('get_inventory'))
         self.assertEqual(response.status_code, 200)
         self.assertIn('inventory', response.json())
+        inventory_data = response.json()['inventory']
+        self.assertEqual(len(inventory_data), 1)
+        self.assertEqual(inventory_data[0]['location'], self.inventory_item.location)
+        self.assertEqual(inventory_data[0]['qty'], self.inventory_item.qty)
+        self.assertEqual(inventory_data[0]['warehouse_number'], self.inventory_item.warehouse_number)
+        self.assertEqual(inventory_data[0]['amount_needed'], self.inventory_item.amount_needed)
 
     def test_add_inventory_item(self):
         data = {
@@ -37,6 +43,11 @@ class InventoryTests(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn('message', response.json())
         self.assertEqual(response.json()['message'], "Item added successfully")
+        
+        new_item = Inventory.objects.get(location="Test ADD")
+        self.assertEqual(new_item.qty, 200)
+        self.assertEqual(new_item.warehouse_number, "499 B")
+        self.assertEqual(new_item.amount_needed, 100)
 
     def test_delete_inventory_items(self):
         item_ids = [self.inventory_item.inventory_id]
@@ -45,3 +56,6 @@ class InventoryTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('message', response.json())
         self.assertEqual(response.json()['message'], "Items deleted successfully")
+        
+        with self.assertRaises(Inventory.DoesNotExist):
+            Inventory.objects.get(inventory_id=self.inventory_item.inventory_id)
